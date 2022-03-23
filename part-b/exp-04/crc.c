c5069536ea82b4aed8af29da91ea796f2934db5a
Milind Mishra <milind.mishra4@gmail.com>
2022-03-24 03:35:02 +0530

    🐛 FIX: new

--- part-b/exp-04/crc.c
+++ part-b/exp-04/crc.c
@@ -1,61 +1,63 @@
 //EXP-4 - CRC-CCITT Polynomial to obtain CRC Code////
+// CRC : Cyclic Redundancy Check - data error detection
+// CCITT : Consultative Committee for International Telephony and Telegraphy
 
 #include<stdio.h>
 int a[100], b[100], i, j, len, k, count = 0;
 
 //Generator Polynomial:g(x)=x^16+x^12+x^5+1
 
-int gp[] = { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, };
+int gp[] = { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 
-int main ()
+int main()
 {
-  void div ();
-  printf ("\nEnter the length of Data Frame :");
-  scanf ("%d", &len);
-  printf ("\nEnter the Message :");
+  void div();
+  printf("\nEnter the length of Data Frame :");
+  scanf("%d", &len);
+  printf("\nEnter the Message :");
   for (i = 0; i < len; i++)
-    scanf ("%d", &a[i]);
+    scanf("%d", &a[i]);
 
-//Append r (16) degree Zeros to Msg bits//
+  //Append r (16) degree Zeros to Msg bits//
 
   for (i = 0; i < 16; i++)
     a[len++] = 0;
-//Xr.M(x)  (ie. Msg+16 Zeros)
+  //Xr.M(x)  (ie. Msg+16 Zeros)
   for (i = 0; i < len; i++)
     b[i] = a[i];
-//No of times to be divided ie. Message Length
+  //No of times to be divided ie. Message Length
   k = len - 16;
-  div ();
+  div();
   for (i = 0; i < len; i++)
     b[i] = b[i] ^ a[i];		//MOD 2 Subtraction
-  printf ("\nData to be transmitted : ");
+  printf("\nData to be transmitted : ");
   for (i = 0; i < len; i++)
-    printf ("%2d", b[i]);
+    printf("%2d", b[i]);
 
-  printf ("\n\nEnter the Reveived Data : ");
+  printf("\n\nEnter the Reveived Data : ");
   for (i = 0; i < len; i++)
-    scanf ("%d", &a[i]);
+    scanf("%d", &a[i]);
 
-  div ();
+  div();
 
   for (i = 0; i < len; i++)
     if (a[i] != 0)
-      {
-	printf ("\nERROR in Recived Data");
-	return 0;
-      }
-  printf ("\nData Recived is ERROR FREE");
+    {
+      printf("\nERROR in Recived Data");
+      return 0;
+    }
+  printf("\nData Recived is ERROR FREE");
 }
 
-void div ()
+void div()
 {
   for (i = 0; i < k; i++)
+  {
+    if (a[i] == gp[0])
     {
-      if (a[i] == gp[0])
-	    {
-	    for (j = i; j < 17 + i; j++)
-	        a[j] = a[j] ^ gp[count++];
-	    }
-        count = 0;
+      for (j = i; j < 17 + i; j++)
+        a[j] = a[j] ^ gp[count++];
     }
+    count = 0;
+  }
 }
